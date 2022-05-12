bde415911478b28a233ee9d20194a3bae57e834a
RaphaelOlivier <raphael.franck.olivier@gmail.com>
2022-05-12 18:49:39 -0400

    remove obsolete features and dependencies

--- dl_utils/spatial_smoothing_pytorch.py
+++ dl_utils/spatial_smoothing_pytorch.py
@@ -31,7 +31,6 @@ from typing import Optional, Tuple, TYPE_CHECKING
 
 import numpy as np
 
-from art.defences.preprocessor.preprocessor import PreprocessorPyTorch
 
 if TYPE_CHECKING:
     # pylint: disable=C0412
@@ -41,7 +40,7 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-class SpatialSmoothingPyTorch(PreprocessorPyTorch):
+class SpatialSmoothingPyTorch:
     """
     Implement the local spatial smoothing defence approach in PyTorch.
 
@@ -74,7 +73,7 @@ class SpatialSmoothingPyTorch(PreprocessorPyTorch):
         """
         import torch  # lgtm [py/repeated-import]
 
-        super().__init__(apply_fit=apply_fit, apply_predict=apply_predict)
+        #super().__init__(apply_fit=apply_fit, apply_predict=apply_predict)
 
         self.channels_first = channels_first
         self.window_size = window_size
@@ -108,7 +107,8 @@ class SpatialSmoothingPyTorch(PreprocessorPyTorch):
                     # kornia >= 0.5.0
                     from kornia.filters.median import _compute_zero_padding
 
-                    padding = _compute_zero_padding(kernel_size)  # type: ignore
+                    padding = _compute_zero_padding(
+                        kernel_size)  # type: ignore
                 self.p2d = [
                     int(padding[-1]) + half_pad[-1],  # type: ignore
                     int(padding[-1]),  # type: ignore
@@ -129,12 +129,15 @@ class SpatialSmoothingPyTorch(PreprocessorPyTorch):
                 import torch.nn.functional as F
 
                 if not torch.is_tensor(input):
-                    raise TypeError("Input type is not a torch.Tensor. Got {}".format(type(input)))
+                    raise TypeError(
+                        "Input type is not a torch.Tensor. Got {}".format(type(input)))
                 if not len(input.shape) == 4:
-                    raise ValueError("Invalid input shape, we expect BxCxHxW. Got: {}".format(input.shape))
+                    raise ValueError(
+                        "Invalid input shape, we expect BxCxHxW. Got: {}".format(input.shape))
                 # prepare kernel
                 batch_size, channels, height, width = input.shape
-                kernel: torch.Tensor = self.kernel.to(input.device).to(input.dtype)
+                kernel: torch.Tensor = self.kernel.to(
+                    input.device).to(input.dtype)
                 # map the local window to single vector
 
                 _input = input.reshape(batch_size * channels, 1, height, width)
@@ -149,14 +152,16 @@ class SpatialSmoothingPyTorch(PreprocessorPyTorch):
                     _input = F.pad(_input, self.p2d, "reflect")
 
                 features: torch.Tensor = F.conv2d(_input, kernel, stride=1)
-                features = features.view(batch_size, channels, -1, height, width)  # BxCx(K_h * K_w)xHxW
+                features = features.view(
+                    batch_size, channels, -1, height, width)  # BxCx(K_h * K_w)xHxW
 
                 # compute the median along the feature axis
                 # * torch.median(), if window size even, use smaller value (e.g. median(4,5)=4)
                 median: torch.Tensor = torch.median(features, dim=2)[0]
                 return median
 
-        self.median_blur = MedianBlurCustom(kernel_size=(self.window_size, self.window_size))
+        self.median_blur = MedianBlurCustom(
+            kernel_size=(self.window_size, self.window_size))
 
     def forward(
         self, x: "torch.Tensor", y: Optional["torch.Tensor"] = None
@@ -176,11 +181,13 @@ class SpatialSmoothingPyTorch(PreprocessorPyTorch):
             if self.channels_first:
                 # NCFHW --> NFCHW --> NCHW
                 nb_clips, channels, clip_size, height, width = x.shape
-                x_nchw = x.permute(0, 2, 1, 3, 4).reshape(nb_clips * clip_size, channels, height, width)
+                x_nchw = x.permute(0, 2, 1, 3, 4).reshape(
+                    nb_clips * clip_size, channels, height, width)
             else:
                 # NFHWC --> NHWC --> NCHW
                 nb_clips, clip_size, height, width, channels = x.shape
-                x_nchw = x.reshape(nb_clips * clip_size, height, width, channels).permute(0, 3, 1, 2)
+                x_nchw = x.reshape(nb_clips * clip_size,
+                                   height, width, channels).permute(0, 3, 1, 2)
         else:
             raise ValueError(
                 "Unrecognized input dimension. Spatial smoothing can only be applied to image and video data."
@@ -198,12 +205,14 @@ class SpatialSmoothingPyTorch(PreprocessorPyTorch):
         elif x_ndim == 5:  # lgtm [py/redundant-comparison]
             if self.channels_first:
                 # NCFHW <-- NFCHW <-- NCHW
-                x_nfchw = x_nchw.reshape(nb_clips, clip_size, channels, height, width)
+                x_nfchw = x_nchw.reshape(
+                    nb_clips, clip_size, channels, height, width)
                 x = x_nfchw.permute(0, 2, 1, 3, 4)
             else:
                 # NFHWC <-- NHWC <-- NCHW
                 x_nhwc = x_nchw.permute(0, 2, 3, 1)
-                x = x_nhwc.reshape(nb_clips, clip_size, height, width, channels)
+                x = x_nhwc.reshape(nb_clips, clip_size,
+                                   height, width, channels)
 
         if self.clip_values is not None:
             x = x.clamp(min=self.clip_values[0], max=self.clip_values[1])
@@ -215,10 +224,11 @@ class SpatialSmoothingPyTorch(PreprocessorPyTorch):
             raise ValueError("Sliding window size must be a positive integer.")
 
         if self.clip_values is not None and len(self.clip_values) != 2:
-            raise ValueError("'clip_values' should be a tuple of 2 floats or arrays containing the allowed data range.")
+            raise ValueError(
+                "'clip_values' should be a tuple of 2 floats or arrays containing the allowed data range.")
 
         if self.clip_values is not None and np.array(self.clip_values[0] >= self.clip_values[1]).any():
             raise ValueError("Invalid 'clip_values': min >= max.")
 
-    def __call__(self,x,*args,**kwargs):
-        return self.forward(x,*args,**kwargs)
+    def __call__(self, x, *args, **kwargs):
+        return self.forward(x, *args, **kwargs)
--- dl_utils/transforms.py
+++ dl_utils/transforms.py
@@ -1,56 +1,55 @@
-import torch 
-import numpy as np
-import torchvision
-import torch.nn as nn 
-import random
-import os
-import dill
-import sys 
 from robustness import model_utils
-from scripts.paths import DIFFJPEG_PATH
-sys.path.append(DIFFJPEG_PATH)
+import sys
+import dill
+import os
+import random
+import torch.nn as nn
+import torchvision
+import numpy as np
+import torch
 from DiffJPEG import DiffJPEG
-from angles import metrics_utils
+from typing import Optional, Tuple
 from dl_utils.spatial_smoothing_pytorch import SpatialSmoothingPyTorch
-from art.defences.preprocessor.thermometer_encoding import ThermometerEncoding
-from art.utils import to_categorical
-from art.config import ART_NUMPY_DTYPE
+from angles import metrics_utils
 
-from typing import Optional, Tuple, TYPE_CHECKING
-if TYPE_CHECKING:
-    from art.utils import CLIP_VALUES_TYPE
-NUM_SPACE_THERMO = 10
+
 class ModuleWithPreprocessing(nn.Module):
-    def __init__(self,preprocessor,model):
-        super(ModuleWithPreprocessing,self).__init__()
-        self.preprocessor=preprocessor
-        self.model=model
-    def forward(self,x,*args,**kwargs):
-        h = self.preprocessor(x,*args,**kwargs)
-        if isinstance(h,tuple):
-            h=h[0]
-        h = self.model(h,*args,**kwargs)
+    def __init__(self, preprocessor, model):
+        super(ModuleWithPreprocessing, self).__init__()
+        self.preprocessor = preprocessor
+        self.model = model
+
+    def forward(self, x, *args, **kwargs):
+        h = self.preprocessor(x, *args, **kwargs)
+        if isinstance(h, tuple):
+            h = h[0]
+        h = self.model(h, *args, **kwargs)
         return h
+
 
 class JPEG(DiffJPEG):
-    def forward(self,x,*args,**kwargs):
-        h = super(JPEG,self).forward(x).contiguous()
+    def forward(self, x, *args, **kwargs):
+        h = super(JPEG, self).forward(x).contiguous()
         return h
-    def __call__(self,x,*args,**kwargs):
-        return self.forward(x,*args,**kwargs)
+
+    def __call__(self, x, *args, **kwargs):
+        return self.forward(x, *args, **kwargs)
+
 
 class Randomizer(nn.Module):
-    def forward(self,x,*args,**kwargs):
-        rand_w = random.randint(30,38)
-        rand_h = random.randint(30,38)
-        trf_1 = torchvision.transforms.Resize(size=(rand_w,rand_h))
-        rand_pads = [random.randint(0,5) for _ in range(4)]
+    def forward(self, x, *args, **kwargs):
+        rand_w = random.randint(30, 38)
+        rand_h = random.randint(30, 38)
+        trf_1 = torchvision.transforms.Resize(size=(rand_w, rand_h))
+        rand_pads = [random.randint(0, 5) for _ in range(4)]
         trf_2 = torchvision.transforms.functional.pad
         x = trf_1(x)
-        x = trf_2(x,rand_pads)
+        x = trf_2(x, rand_pads)
         return x
-    def __call__(self,x,*args,**kwargs):
-        return self.forward(x,*args,**kwargs)
+
+    def __call__(self, x, *args, **kwargs):
+        return self.forward(x, *args, **kwargs)
+
 
 class DiffRound(torch.autograd.Function):
     @staticmethod
@@ -73,93 +72,6 @@ class DiffRound(torch.autograd.Function):
         grad_input = grad_output.clone()
         return grad_input
 
-
-def np_thermo(x: np.ndarray, y=None):
-    # Now apply the encoding:
-    channel_index = 1
-    result = np.apply_along_axis(_perchannel, channel_index, x)
-    np.clip(result, 0, 1, out=result)
-    return result.astype(ART_NUMPY_DTYPE)
-
-def _perchannel(x: np.ndarray):
-    """
-    Apply thermometer encoding to one channel.
-
-    :param x: Sample to encode with shape `(batch_size, width, height)`.
-    :return: Encoded sample with shape `(batch_size, width, height, num_space)`.
-    """
-    pos = np.zeros(shape=x.shape)
-    for i in range(1, NUM_SPACE_THERMO):
-        pos[x > float(i) / NUM_SPACE_THERMO] += 1
-
-    onehot_rep = to_categorical(pos.reshape(-1), NUM_SPACE_THERMO)
-
-    for i in range(NUM_SPACE_THERMO - 1):
-        onehot_rep[:, i] += np.sum(onehot_rep[:, i + 1 :], axis=1)
-
-    return onehot_rep.flatten()
-
-def np_thermo_gradient(x: np.ndarray, grad: np.ndarray):
-    """
-    Provide an estimate of the gradients of the defence for the backward pass. For thermometer encoding,
-    the gradient estimate is the one used in https://arxiv.org/abs/1802.00420, where the thermometer encoding
-    is replaced with a differentiable approximation:
-    `g(x_{i,j,c})_k = min(max(x_{i,j,c} - k / self.num_space, 0), 1)`.
-
-    :param x: Input data for which the gradient is estimated. First dimension is the batch size.
-    :param grad: Gradient value so far.
-    :return: The gradient (estimate) of the defence.
-    """
-    x = np.transpose(x, (0,) + tuple(range(2, len(x.shape))) + (1,))
-    grad = np.transpose(grad, (0,) + tuple(range(2, len(x.shape))) + (1,))
-
-    thermometer_grad = np.zeros(x.shape[:-1] + (x.shape[-1] * NUM_SPACE_THERMO,))
-    mask = np.array([x > k / NUM_SPACE_THERMO for k in range(NUM_SPACE_THERMO)])
-    mask = np.moveaxis(mask, 0, -1)
-    mask = mask.reshape(thermometer_grad.shape)
-    thermometer_grad[mask] = 1
-
-    grad = grad * thermometer_grad
-    grad = np.reshape(grad, grad.shape[:-1] + (grad.shape[-1] // NUM_SPACE_THERMO, NUM_SPACE_THERMO))
-    grad = np.sum(grad, -1)
-
-    x = np.transpose(x, (0,) + (len(x.shape) - 1,) + tuple(range(1, len(x.shape) - 1)))
-    grad = np.transpose(grad, (0,) + (len(x.shape) - 1,) + tuple(range(1, len(x.shape) - 1)))
-
-    return grad
-
-
-class DiffThermEnc(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input):
-        """
-        In the forward pass we receive a Tensor containing the input and return
-        a Tensor containing the output. ctx is a context object that can be used
-        to stash information for backward computation. You can cache arbitrary
-        objects for use in the backward pass using the ctx.save_for_backward method.
-        """
-        ctx.input = input
-        x_np = input.detach().cpu().numpy()
-        x_enc_np = np_thermo(x_np)
-        x_enc = torch.tensor(x_enc_np).to(input.device)
-        return x_enc
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        """
-        In the backward pass we receive a Tensor containing the gradient of the loss
-        with respect to the output, and we need to compute the gradient of the loss
-        with respect to the input.
-        """
-        grad = grad_output.detach().cpu().numpy()
-        input = ctx.input.detach().cpu().numpy()
-        grad_input = np_thermo_gradient(input,grad)
-        return torch.tensor(grad_input).to(grad_output.device)
-
-
-class ThermometerEncodingPytorch(nn.Module):
-    def forward(self, x):
-        return DiffThermEnc.apply(x)
 
 class FeatureSqueezing(nn.Module):
     def __init__(
@@ -192,28 +104,25 @@ class FeatureSqueezing(nn.Module):
         return res
 
 
-def add_jpeg_preprocessing(model,size,quality=50, device = "cuda"):
+def add_jpeg_preprocessing(model, size, quality=50, device="cuda"):
     jpeg = JPEG(height=size, width=size, differentiable=True, quality=quality)
     jpeg.to(device)
-    return ModuleWithPreprocessing(jpeg,model)
+    return ModuleWithPreprocessing(jpeg, model)
 
-def add_random_preprocessing(model, device = "cuda"):
+
+def add_random_preprocessing(model, device="cuda"):
     rd = Randomizer()
     rd.to(device)
-    return ModuleWithPreprocessing(rd,model)
+    return ModuleWithPreprocessing(rd, model)
 
-def add_squeezing_preprocessing(model,depth=1, device = "cuda"):
+
+def add_squeezing_preprocessing(model, depth=1, device="cuda"):
     fs = FeatureSqueezing(bit_depth=depth)
     fs.to(device)
-    return ModuleWithPreprocessing(fs,model)
-
-def add_spatial_preprocessing(model,window_size=3):
-    sps = SpatialSmoothingPyTorch(window_size=window_size,channels_first=True)
-    #sps.to(device)
-    return ModuleWithPreprocessing(sps,model)
+    return ModuleWithPreprocessing(fs, model)
 
 
-def add_thermo_preprocessing(model,window_size=3):
-    th = ThermometerEncodingPytorch()
-    #sps.to(device)
-    return ModuleWithPreprocessing(th,model)
+def add_spatial_preprocessing(model, window_size=3):
+    sps = SpatialSmoothingPyTorch(window_size=window_size, channels_first=True)
+    # sps.to(device)
+    return ModuleWithPreprocessing(sps, model)
