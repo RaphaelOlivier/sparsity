b2efb692d62af7dd31bbd728a98fc98f8282daf7
RaphaelOlivier <raphael.franck.olivier@gmail.com>
2022-08-18 06:57:49 -0400

    remove hard links

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,6 +1,6 @@
-CIFAR_PATH = '/home/raphael/dataspace/datasets/cifar-10-batches-py'
-DIFFJPEG_PATH = '/home/raphael/workspace/libs/DiffJPEG'
-OUTPUT_FOLDER_PATH = '/home/raphael/dataspace/outputs/easy_problem_attack'
-AUG_PATH = '/home/raphael/dataspace/datasets/cifar-10-batches-py/cifar10_ddpm.npz'
-MNIST_PATH = '/home/raphael/dataspace/datasets/mnist'
-IMAGENET_PATH = '/home/raphael/dataspace/datasets/imagenette2'
+CIFAR_PATH = None
+DIFFJPEG_PATH = None
+OUTPUT_FOLDER_PATH = None
+AUG_PATH = None
+MNIST_PATH = None
+IMAGENET_PATH = None
